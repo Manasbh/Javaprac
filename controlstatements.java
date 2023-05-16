ba5176d31e2dfd091a7479c36d407d6622ea24d5
Manbruv <107252808+Manasbh@users.noreply.github.com>
2023-05-16 16:31:04 +0530

    up

---
+++ controlstatements.java
@@ -0,0 +1,25 @@
+public class controlstatements {
+
+    public static void main(String[] args){
+
+        int tint = 100;
+
+
+        /*IF STATEMENTS
+         * if(conditon)statements;
+         */
+
+         if(tint>60)System.out.println("Test successfull");
+
+
+
+         /*for(initialization; condition; iteration) statement;
+          * 
+          */
+
+          int x;
+
+          for(x=0;x<10;x=x+1)System.out.println("abc");
+    }
+    
+}
