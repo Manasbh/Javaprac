d997175c0f65271a06784eb0c397a078ea172816
Manbruv <107252808+Manasbh@users.noreply.github.com>
2023-05-16 15:58:20 +0530

    Boilerplate
    Boilerplate is the initialization code

---
+++ boilerplate.java
@@ -0,0 +1,11 @@
+/*all code to be written in class */
+
+class boilerplate{
+    
+    /*main method is used as entry point */
+    public static void main(String[] args){
+
+        System.out.println("TestLine1");
+
+    }
+}
